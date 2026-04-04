8996754c7ba6119847ed8e5c3665794315b7965f
Miguel Cortez <miguelcortezcavalcante2005@gmail.com>
2026-04-03 23:32:04 -0300

    feat: add feat: add transaction summary (count and total amount)

--- scr/main.py
+++ scr/main.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import os
 
-arquive_csv = "data/exemplo.csv"
+csv_file = "data/exemplo.csv"
 
 
 try:
-    if not os.path.isfile(arquive_csv):
-        raise FileNotFoundError(f"File not found: {arquive_csv}")
+    if not os.path.isfile(csv_file):
+        raise FileNotFoundError(f"File not found: {csv_file}")
     
-    df = pd.read_csv(arquive_csv, encoding="utf-8", sep =",")
-
+    df = pd.read_csv(csv_file, encoding="utf-8", sep =",")
 
     print("Primeiras linhas do arquivo:")
-    print(df.head())
+    print(df.to_string())
+    montante_final = df["Valor"].sum()
+    number_of_transactions =len(df)
+    print(f"Transactions: {number_of_transactions}")
+    print(f"Transactions: {montante_final}")
+
 except FileNotFoundError as e:
     print(e)
 except pd.errors.EmptyDataError:
@@ -21,3 +25,6 @@ except pd.errors.ParserError:
     print("Erro ao processar o arquivo CSV. Verifique o formato.")
 except Exception as e:
     print(f"Ocorreu um erro inesperado: {e}")
+
+
+
